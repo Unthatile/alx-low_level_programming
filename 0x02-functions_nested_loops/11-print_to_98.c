2df4cc00fbc9dcfd8606a7ea57117618513e0597
Unthatile_Makhajane <umakhajane@yahoo.com>
2023-06-21 18:31:50 -0700

    11. 98 Battery Street, the OG(2)

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,13 +10,13 @@
  */
 void print_to_98(int n)
 {
-while (n <= 98)
+if (n <= 98)
 {
-printf("%d", n);
-if (n != 98)
-printf(", ");
-else
-printf("\n");
-n++;
+for (n = n; n <= 97; n++)
+printf("%d, ", n);
 }
+else
+for (n = n; n > 98; n--)
+printf("%d, ", n);
+printf("98\n");
 }
