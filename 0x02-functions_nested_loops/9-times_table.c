4bb218633684486e2248d613620c446bc2029134
Unthatile_Makhajane <umakhajane@yahoo.com>
2023-06-21 18:03:47 -0700

    9. Learn your times table(2)

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,31 +7,35 @@
  */
 void times_table(void)
 {
-    int i, j;
+	int i, j, n;
 
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            if (j == 0)
-            {
-                _putchar('0');
-            }
-            else if (i * j < 10)
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(' ');
-                _putchar(i * j + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
-                _putchar(i * j / 10 + '0');
-                _putchar(i * j % 10 + '0');
-            }
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			n = i * j;
+
+			if ((n / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(n + '0');
+
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (j == 9)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
