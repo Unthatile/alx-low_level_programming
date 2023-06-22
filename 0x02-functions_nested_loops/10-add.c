9901cdc487135a6735e03be8eae52059c07d3943
Unthatile_Makhajane <umakhajane@yahoo.com>
2023-06-21 18:12:36 -0700

    10. a + b(2)

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,14 @@
 /**
  * add - adds two integrers and returns the result
  *
- * @x: number being added
+ * @a: number being added
  *
- * @y: number being added
+ * @b: number being added
  *
  * Return: Always 0.
  *
  */
 int add(int a, int b)
 {
-    return a + b;
+return (a + b);
 }
