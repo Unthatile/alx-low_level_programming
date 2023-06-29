a8258f33456278009c958a4118e5ad6851ab9d58
Unthatile_Makhajane <umakhajane@yahoo.com>
2023-06-29 14:05:50 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes(2)

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,40 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
- * Return: A pointer to the changed string.
+ * cap_string - function that capitalizes all words of a string
+ * @str: String to capitalize
+ *
+ * Return: pointer to str
  */
+
 char *cap_string(char *str)
 {
-	int index = 0;
+	int i, j, found;
+	int sep[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (str[index])
+	i = 0;
+	found = 0;
+	if ((str[i] >= 97) && (str[i] <= 122))
+		*(str + i) -= 32;
+
+	i++;
+
+	while (str[i] != '\0')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		str[index - 1] == '\t' ||
-		str[index - 1] == '\n' ||
-		str[index - 1] == ',' ||
-		str[index - 1] == ';' ||
-		str[index - 1] == '.' ||
-		str[index - 1] == '!' ||
-		str[index - 1] == '?' ||
-		str[index - 1] == '"' ||
-		str[index - 1] == '(' ||
-		str[index - 1] == ')' ||
-		str[index - 1] == '{' ||
-		str[index - 1] == '}' ||
-		index == 0)
-			str[index] -= 32;
-		index++;
+		j = 0;
+		while (j < 13)
+		{
+			if (sep[j] == str[i])
+			{
+				found = 1;
+				break;
+			}
+			j++;
+		}
+		if ((found == 1) && ((str[i + 1] >= 97) && (str[i + 1] <= 122)))
+			*(str + (i + 1)) -= 32;
+		i++;
+		found = 0;
 	}
 	return (str);
 }
