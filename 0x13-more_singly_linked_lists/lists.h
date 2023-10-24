d287d871cbe22aabb9e0b9ded91bf2774f31786c
Unthatile <umakhajane@gmail.com>
2023-10-24 13:43:22 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,7 +10,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for alx project
  */
 typedef struct listint_s
 {
@@ -33,5 +32,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+int _puchar(char c);
 
 #endif
